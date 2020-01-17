d1c99ae2a56d70ff161a5e63a5401f8f4578d17d
Chuks Opia <chuks24ng@yahoo.co.uk>
2020-01-17 18:57:44 +0100

    - create model and run migrations

---
+++ db/migrate/20200117173228_create_notes.rb
@@ -0,0 +1,11 @@
+class CreateNotes < ActiveRecord::Migration[6.0]
+  def change
+    create_table :notes do |t|
+      t.string :title
+      t.text :body
+
+      t.timestamps
+    end
+    add_index :notes, :title
+  end
+end
